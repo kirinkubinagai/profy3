2075130cb8e8573ce1c35d9dcf4938581dfe0004
柴田 大介 <oujirou6kg@gmail.com>
2016-10-31 14:50:12 +0900

    initial commit

---
+++ test/controllers/questions_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class QuestionsControllerTest < ActionController::TestCase
+  # def create
+  #   question = Question.create(create_params)
+  #   question.feed_content = FeedContent.create(group_id: question.group_id, updated_at: question.updated_at)
+  #   redirect_to :root and return
+  # end
+end
